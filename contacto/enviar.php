336792c20ae29ba44b6ca3d4b59088755898e33f
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-25 02:09:52 +0530

    idk

--- contacto/enviar.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$destino= "carlosarriagacm@gmail.com";
-	$nombre= $_POST["nombre"];
-	$correo= $_POST["correo"];
-	$mensaje= $_POST["mensaje"];
-	$contenido= "Nombre= " . $nombre . "\nCorreo= " . $correo . "\nMensaje= " . $mensaje;
-	mail($destino, "Mail de la Web", $contenido);
-?>
